49582ee448ef413a9f7f45e8400f645ed66497a5
Deepak Baskar <deepak.baskar@htcinc.com>
2024-07-15 14:13:07 +0530

    Added runner service to initialize application with dependent modules and to package the application as single unit

---
+++ runner-service/src/test/java/in/transportstack/delhi/runnerservice/RunnerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.transportstack.delhi.runnerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
