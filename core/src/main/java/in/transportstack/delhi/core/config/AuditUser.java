f8d793cf1012a08eb0f97582a31ce3d26ee13b89
Deepak Baskar <deepak.baskar@htcinc.com>
2024-07-24 08:51:34 +0530

    Added current audit config and audit user abstract entity

---
+++ core/src/main/java/in/transportstack/delhi/core/config/AuditUser.java
@@ -0,0 +1,23 @@
+package in.transportstack.delhi.core.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditUser implements AuditorAware<String> {
+
+    @Override
+    @NonNull
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            return Optional.of(authentication.getName());
+        }
+        return Optional.of("system");
+    }
+}
