fa8f248f5c57e0cd48ae77be44819b0d4eaa5144
Deepak Baskar <deepak.baskar@htcinc.com>
2024-07-25 16:25:13 +0530

    Configured gateway service with spring security and AWS cognito to secure endpoints

--- gateway-service/src/main/java/in/transportstack/delhi/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/in/transportstack/delhi/gatewayservice/GatewayServiceApplication.java
@@ -1,9 +1,14 @@
 package in.transportstack.delhi.gatewayservice;
 
+import in.transportstack.delhi.sharedconfig.SharedConfigApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({
+        SharedConfigApplication.class
+})
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
---
+++ gateway-service/src/main/java/in/transportstack/delhi/gatewayservice/security/SecurityConfig.java
@@ -0,0 +1,104 @@
+package in.transportstack.delhi.gatewayservice.security;
+
+import com.nimbusds.jose.EncryptionMethod;
+import com.nimbusds.jose.JWEAlgorithm;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.RemoteJWKSet;
+import com.nimbusds.jose.proc.*;
+import com.nimbusds.jose.util.Base64URL;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import com.nimbusds.jwt.proc.JWTProcessor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.URL;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.interfaces.RSAPrivateKey;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JWSAlgorithm jwsAlgorithm = JWSAlgorithm.RS256;
+    private final JWEAlgorithm jweAlgorithm = JWEAlgorithm.RSA_OAEP_256;
+    private final EncryptionMethod encryptionMethod = EncryptionMethod.A256GCM;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri: https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_RZHvCpZHy/.well-known/jwks.json}")
+    URL jwkSetUri;
+
+    @Value("${jwt.jwe-key-value: classpath:rsa-key.priv}")
+    RSAPrivateKey key;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                requests -> requests
+                        .requestMatchers("api/user/login")
+                        .permitAll()
+                    .requestMatchers("/user-management/user/login")
+                    .permitAll()
+                    .requestMatchers("/user-management/register/user")
+                    .permitAll()
+                    .requestMatchers("/user-management/logout")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new CustomJwtAuthenticationConverter());
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return new NimbusJwtDecoder(jwtProcessor());
+    }
+
+    private JWTProcessor<SecurityContext> jwtProcessor() {
+        JWKSource<SecurityContext> jwsJwkSource = new RemoteJWKSet<>(this.jwkSetUri);
+        JWSKeySelector<SecurityContext> jwsKeySelector = new JWSVerificationKeySelector<>(this.jwsAlgorithm,
+            jwsJwkSource);
+
+        JWKSource<SecurityContext> jweJwkSource = new ImmutableJWKSet<>(new JWKSet(rsaKey()));
+        JWEKeySelector<SecurityContext> jweKeySelector = new JWEDecryptionKeySelector<>(this.jweAlgorithm,
+            this.encryptionMethod, jweJwkSource);
+
+        ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
+        jwtProcessor.setJWSKeySelector(jwsKeySelector);
+        jwtProcessor.setJWEKeySelector(jweKeySelector);
+
+        return jwtProcessor;
+    }
+
+    private RSAKey rsaKey() {
+        RSAPrivateCrtKey crtKey = (RSAPrivateCrtKey) this.key;
+        Base64URL n = Base64URL.encode(crtKey.getModulus());
+        Base64URL e = Base64URL.encode(crtKey.getPublicExponent());
+        return new RSAKey.Builder(n, e).privateKey(this.key).keyUse(KeyUse.ENCRYPTION).build();
+    }
+}
