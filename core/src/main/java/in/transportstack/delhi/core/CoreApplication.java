f4407b4361c054f427c471df2c93ee7151d4ff82
Deepak Baskar <deepak.baskar@htcinc.com>
2024-07-25 16:37:41 +0530

    Enabled JPA repositories for submodule global access

--- core/src/main/java/in/transportstack/delhi/core/CoreApplication.java
+++ core/src/main/java/in/transportstack/delhi/core/CoreApplication.java
@@ -3,9 +3,11 @@ package in.transportstack.delhi.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "in.transportstack.delhi")
 public class CoreApplication {
 
     public static void main(String[] args) {
